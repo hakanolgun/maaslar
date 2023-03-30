5ac1ae351d4501db05a046ed73dfb3c909d56bf9
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 15:02:21 +0300

    Add date to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-dark py-4 d-flex justify-content-center ">
+    <div className="bg-dark py-4 d-flex flex-column justify-content-center text-white">
       <Link
         style={{ textDecoration: "none" }}
         className="mb-0 fs-4 text-center text-white"
         href="/"
       >
-        Yazılım Sektörü Ücretler
+        Bilişim Sektörü Ücretler
       </Link>
+      <p className="text-center fs-5 mb-0">2023 Şubat</p>
     </div>
   );
 };
