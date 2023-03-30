65580a426484ec062894ac6862e6b42b930a1cae
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 23:09:58 +0300

    Add .nojekyll to public folder

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   basePath: "/maaslar",
   experimental: {
     appDir: false,
+  },
+  assetPrefix: isProd ? "/maaslar/" : "",
+  images: {
+    unoptimized: true,
   },
 };
 
