5b105699f1ab3ad407be7683ebb12b34b37fdadb
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 20:16:03 +0300

    Create Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footerContainer text-center">
+      <p>
+        Bu proje açık kaynak olarak{" "}
+        <a className="text-info" href="https://github.com/hakanolgun/maaslar">
+          github hesabımda
+        </a>{" "}
+        bulunmaktadır.
+      </p>
+      <p>İsteyen herkes kullanabilir, katkıda bulunabilir</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,10 +1,12 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-export default function Layout({ children }) {
+export default function Layout({ children }: any) {
   return (
-    <main className="pb-3" style={{ backgroundColor: "black" }}>
+    <main className="h-100" style={{ backgroundColor: "black" }}>
       <Header />
       {children}
+      <Footer />
     </main>
   );
 }
