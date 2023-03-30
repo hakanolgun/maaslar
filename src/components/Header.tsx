8a4d97185e8ea527274a5ea0ef191b1e3f4d9a59
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 22:10:01 +0300

    Add hover effect to header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,9 @@ const Header = () => {
   return (
     <div className="bg-dark py-4 d-flex flex-column justify-content-center">
       <Link style={{ textDecoration: "none" }} href="/">
-        <h1 className="text-center h1 text-purple">Bilişim Sektörü Ücretler</h1>
+        <h1 className="text-center h1 text-purple hvr">
+          Bilişim Sektörü Ücretler
+        </h1>
       </Link>
       <p className="text-center text-warning mb-0">2023 Şubat</p>
     </div>
