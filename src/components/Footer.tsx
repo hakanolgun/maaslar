da9a74bd9c94fbf2cc7af5b7fc085461d3a99b26
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 21:59:54 +0300

    Change footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="footerContainer text-center">
-      <p>
+    <div className="footerContainer text-center bg-dark">
+      <p className="m-0">
         Bu proje açık kaynak olarak{" "}
         <a
           className="text-info"
           href="https://github.com/hakanolgun/maaslar"
           target="_blank"
         >
-          github hesabımda
+          github hesabımdan
         </a>{" "}
-        bulunmaktadır.
+        erişebilirsiniz.
       </p>
-      <p>İsteyen herkes kullanabilir, katkıda bulunabilir</p>
     </div>
   );
 };
