ac1baea006369b64471b5bedb34651579b13bfe6
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 23:52:24 +0300

    Change footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footerContainer text-center bg-dark">
       <p className="m-0">
-        Bu proje açık kaynak olarak{" "}
+        Bu projeye{" "}
         <a
           className="text-info"
           href="https://github.com/hakanolgun/maaslar"
@@ -12,7 +12,7 @@ const Footer = () => {
         >
           github hesabımdan
         </a>{" "}
-        erişebilirsiniz.
+        erişebilir, kullanabilir veya katkıda bulunabilirsiniz.
       </p>
     </div>
   );
