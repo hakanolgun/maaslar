5e26088e48643f2b8cb7f5a20fa200c5cb7e7578
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 12:19:46 +0300

    Configure NextUI and bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import { useEffect } from "react";
+import { store } from "@/store/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap" as never);
+  }, []);
+  return (
+    <Provider store={store}>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </Provider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,44 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { CssBaseline } from "@nextui-org/react";
+import Header from "@/components/Header";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles]),
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {CssBaseline.flush()}
+          <meta name="description" content="Yazılım sektörü ücret bilgileri" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/react-bootstrap-typeahead/css/Typeahead.css"
+          />
+        </Head>
+        <body>
+          <Header />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
