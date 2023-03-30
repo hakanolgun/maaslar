28dcdf8315d74cf5d8ec9717f629b03823369e48
hakanolgun <hakanolgun@tutamail.com>
2023-03-30 12:21:46 +0300

    Create IData and IFilter types

---
+++ src/types/types.ts
@@ -0,0 +1,19 @@
+export interface IData {
+  level: string;
+  position: string;
+  tech_stack: string;
+  experience: string;
+  gender: string;
+  company: string;
+  company_size: string;
+  work_type: string;
+  city: string;
+  currency: string;
+  salary: string;
+  raise_period: string;
+}
+
+export interface IFilter {
+  level: string[];
+  position: string[];
+}
